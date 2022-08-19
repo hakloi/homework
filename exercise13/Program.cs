a7381d3b22f5bccb7dcd624aadd04d9a5b5ac814
omezhka <murasakitta@gmail.com>
2022-08-19 13:51:01 +0300

    Программа, которая показывает третью цифру числа

---
+++ exercise13/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет. 
+*/
+
+Console.Clear();
+void GetRandomNumberThird()
+{
+    int randomValue = new Random().Next(10, 999);
+    Console.WriteLine($"Рандомное число: {randomValue}!");
+
+    if (randomValue > 10 && randomValue < 100) 
+    {
+        Console.WriteLine($"Число {randomValue} является двухзначным.");
+        Console.WriteLine("Третьей цифры нет.");
+    } 
+    else 
+    {
+        int thirdNumber = randomValue % 10;
+        Console.WriteLine($"Из числа {randomValue} мы выбираем тратью цифру - {thirdNumber}!");
+    }
+}
+GetRandomNumberThird();
+GetRandomNumberThird();
