bedcf46c77066146b1f06072df7661a94ab8db17
omezhka <murasakitta@gmail.com>
2022-08-19 13:21:54 +0300

    Вторая цифра из трехзначного числа

--- exercise10/Program.cs
+++ exercise10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+*/
+
+Console.Clear();
+void GetRandomNumberSecond()
+{
+    int randomValue = new Random().Next(100, 999);
+    Console.WriteLine($"Рандомное число: {randomValue}!");
+
+    int thirdNumber = randomValue % 10;
+    int secondNumber = randomValue % 100 / 10;
+    int firstNumber = randomValue / 100;
+
+    Console.WriteLine($"Из числа {randomValue} мы выбираем вторую цифру - {secondNumber}!");
+}
+GetRandomNumberSecond();
+GetRandomNumberSecond();
