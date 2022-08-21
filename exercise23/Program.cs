adf1bd9467a9c9c2449b5c5696a30b28c0a200a9
omezhka <murasakitta@gmail.com>
2022-08-21 19:51:29 +0300

    Возведение числа в куб

---
+++ exercise23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= number)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+    i++;
+}
