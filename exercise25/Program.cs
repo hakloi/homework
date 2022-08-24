dea94de25be0ea9ede82736005d60e424b150d6e
omezhka <murasakitta@gmail.com>
2022-08-24 09:59:26 +0300

    Возведение в степень

---
+++ exercise25/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень, в которую необходимо возвести число: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int ToThePowerOf(int num, int pow)
+{
+    int result = 1;
+    for (int i = 1; i <= pow; i++)
+    {
+        result = result * num;
+    }
+    return result;
+}
+
+int res = ToThePowerOf(number, power);
+Console.WriteLine($"При возведении числа {number} в {power} степень, получим: {res}.");
