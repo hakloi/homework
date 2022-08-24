354c7cbb100e807a9e3bfaba2f3781217d2d9b28
omezhka <murasakitta@gmail.com>
2022-08-24 12:24:46 +0300

    Массив из 8 элементов

---
+++ exercise29/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+int[] array = new int[8];
+
+int[] GetArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите число для создания массива: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+int[] res = GetArray(array);
+PrintArray(res);
