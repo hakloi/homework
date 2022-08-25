6bce984b6cb35a4c5c82e250f405a0d0314a8c65
omezhka <murasakitta@gmail.com>
2022-08-25 21:17:39 +0300

    Массив трехзначных чисел, показывает количество положительных

---
+++ exercise34/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 34: Задайте массив заполненный случайными
+положительными трёхзначными числами. Напишите
+программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+Console.Clear();
+int[] array = new int[6];
+
+int[] RandomArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    string text = "\nМассив: ";
+    Console.Write(text);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] oldArray = RandomArray(array);
+PrintArray(oldArray);
+
+int EvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int howManyNumbers = EvenNumbers(array);
+string space = "\n";
+Console.WriteLine(space + $"В массиве {howManyNumbers} положительных чисел!");
